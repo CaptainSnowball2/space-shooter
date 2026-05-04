75e9ba6709290744446784e50b7761d1cf6995bf
CaptainSnowball2 <arno.kruger2013@outlook.com>
2026-05-04 17:55:43 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -15,6 +15,8 @@ load2R=pygame.transform.rotate(load2S,270)
 load3R=pygame.transform.rotate(load3S,90)
 YS=pygame.Rect(248,300,60,60)
 RS=pygame.Rect(753,300,60,60)
+shoot=pygame.mixer.Sound("space shooter/images/Gun+Silencer.mp3")
+YB=[]
 def YR():
     if keys_pressed[pygame.K_w] and YS.y>0:
         YS.y-=2.5
@@ -24,6 +26,15 @@ def YR():
         YS.y+=2.5
     if keys_pressed[pygame.K_d] and YS.x<435:
         YS.x+=2.5
+def RR():
+    if keys_pressed[pygame.K_UP] and RS.y>0:
+        RS.y-=2.5
+    if keys_pressed[pygame.K_LEFT] and RS.x>505:
+        RS.x-=2.5
+    if keys_pressed[pygame.K_DOWN] and RS.y<540:
+        RS.y+=2.5
+    if keys_pressed[pygame.K_RIGHT] and RS.x<940:
+        RS.x+=2.5
 clock = pygame.time.Clock()
 while True:
     clock.tick(FPS)
@@ -32,12 +43,25 @@ while True:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_KP_0:
+                shoot.play()
+                bullet2=pygame.Rect(RS.x,RS.y+25,20,10)
+            if event.key == pygame.K_SPACE:
+                shoot.play() 
+                bullet=pygame.Rect(YS.x+60,YS.y+25,20,10)
+                YB.append(bullet)
     keys_pressed = pygame.key.get_pressed()
     YR()
+    RR()
     screen.blit(load3R,YS)
     screen.blit(load2R,RS)
     Ytext=font1.render("health ",True,"white")
     Rtext=font1.render("health ",True,"white")
     screen.blit(Ytext,(10,10))  
     screen.blit(Rtext,(860,10)) 
+    for bullet in YB:
+        pygame.draw.rect(screen,"Yellow",bullet)
+        bullet.x+=2
+
     pygame.display.update()
